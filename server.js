3ac0fed6d6c3b40869bf555736e737da86285b11
ML <mlcastaneda@galileo.edu>
2021-04-19 00:09:17 -0600

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -14,6 +18,11 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use('/', indexRouter);
+app.use(bodyParser.urlencoded({
+    limit: '20mb',
+    extended: false
+
+}))
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
@@ -24,6 +33,7 @@ mongoose.connect(process.env.DATABASE_URL, {
     db.once('open', ()=> console.log('Connected to Mongoose'));
 
     app.use('/', indexRouter);
+    app.use('/authors', authorRouter);
 
 
 app.listen(process.env.PORT || 3000);
